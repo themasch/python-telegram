a35d22c775d869e38113e155f0269fee38eef786
Mark Schmale <masch@masch.it>
2014-10-21 18:40:19 +0200

    made stuff usefull

---
+++ Message.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from time import strptime
+import re
+
+__author__ = 'Mark Schmale <masch@masch.it>'
+
+
+class Message():
+    def __init__(self, msgid, timestamp, sender, msg, channel=""):
+        self.received = datetime.utcnow()
+        self.time = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S %z")
+        self.sender = sender
+        self.channel = channel
+        self.message = msg
+
+    def __str__(self):
+        return "[{time}|{received}] [{sender} @ {channel}] : {message}".format(
+            time=self.time,
+            received=self.received,
+            sender=self.sender,
+            channel=self.channel,
+            message=self.message
+        )
+
+
+
--- TelegramClient.py
+++ TelegramClient.py
@@ -1,14 +1,19 @@
+# coding=utf-8
 from TelegramConnection import TelegramConnection
+from Message import Message
 import re
 
 class TelegramClient(TelegramConnection):
 
     def __init__(self, host, port):
         TelegramConnection.__init__(self, host, port)
+        self.re_grp_msg = None
+        self.re_msg = None
         self.compileRe()
 
     def compileRe(self):
-        self.re_msg = re.compile(r"(?P<msgid>\d+)\s+\[(.*)\]\s+")
+        self.re_grp_msg = re.compile(r"(?P<msgid>\d+)\s+\[(?P<time>.*)\]\s+(?P<channel>.*) @ (?P<sender>.*) [»>]+ (?P<msg>.*)$")
+        self.re_msg = re.compile(r"(?P<msgid>\d+)\s+\[(?P<time>.*)\]\s+(?P<sender>.*) [»>]+ (?P<msg>.*)$")
 
     def start_main_session(self):
         self.send("main_session\n")
@@ -17,7 +22,41 @@ class TelegramClient(TelegramConnection):
         self.send("msg " + channel.replace(" ", "_") + " " + message)
 
     def handle_answer(self, msg):
-        pass
+        """
+        :type msg str
+        """
+        groups = self.re_msg.match(msg)
+        if groups:
+            mes = Message(
+                groups.group("msgid"),
+                groups.group("time"),
+                groups.group("sender"),
+                groups.group("msg")
+            )
+        groups = self.re_grp_msg.match(msg)
+        if groups:
+            mes = Message(
+                groups.group("msgid"),
+                groups.group("time"),
+                groups.group("sender"),
+                groups.group("msg"),
+                groups.group("channel")
+            )
+
+        if mes:
+            self.on_message(mes)
+        else:
+            print "<<< " + msg
+
+    def on_message(self, message):
+        """
+        :param message:
+        :type message Message
+        :return:
+        """
+
+        print message
+
 
 
 print "starting..."
